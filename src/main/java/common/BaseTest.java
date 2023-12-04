3bff276cdc3eb7fe7a872a1f9ada4fa0d3534634
trangp21799 <trangp21799@gmail.com>
2023-12-04 22:35:16 +0700

    add headless mode

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -3,7 +3,9 @@ package common;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +19,21 @@ public class BaseTest {
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
+            case "h_chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions headlessChrome0ptions = new ChromeOptions();
+                headlessChrome0ptions.addArguments("--headless");
+                driver = new ChromeDriver(headlessChrome0ptions);
+                break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
+                break;
+            case "h_firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions headlessFirefox0ptions = new FirefoxOptions();
+                headlessFirefox0ptions.addArguments("--headless");
+                driver = new FirefoxDriver(headlessFirefox0ptions);
                 break;
             default:
                 throw new RuntimeException("Invalid browser name");
